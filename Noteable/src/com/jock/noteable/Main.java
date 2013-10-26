f8d09939a97066d7d97c22262b6813108d75f5f8
jock <jockmahon@gmail.com>
2013-10-26 12:17:32 +0100

    initial checkin

---
+++ Noteable/src/com/jock/noteable/Main.java
@@ -0,0 +1,35 @@
+package com.jock.noteable;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Main extends Activity
+{
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.layout_main );
+
+		FragmentManager fragMan = getFragmentManager();
+		FragmentTransaction tran = fragMan.beginTransaction();
+		StaffDisplay staff = new StaffDisplay();
+		tran.add( R.id.mainAppFrame, staff );
+		tran.commit();
+
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu( Menu menu )
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate( R.menu.main, menu );
+		return true;
+	}
+
+}
